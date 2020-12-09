aece7438abad3ae1951342f8971dff42b5b62231
YaelSim <58948260+YaelSim@users.noreply.github.com>
2020-12-09 13:21:13 +0200

    check part 2

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ IP_ADDR = '127.0.0.1'
 CONNECTION_LIMIT = 1
 BUFFER_SIZE = 1024
 
+
 def main():
     port = sys.argv[1]
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,7 +20,6 @@ def main():
         server_socket.close()
 
 
-
 def client_acception_and_handling(server_socket):
     eol_index = -4
     eol_str = b'\r\n\r\n'
@@ -28,26 +28,40 @@ def client_acception_and_handling(server_socket):
         client_socket.settimeout(TIMEOUT_VAL)
         while True:
             try:
-                request_data =  client_socket.recv(BUFFER_SIZE)
+                request_data = client_socket.recv(BUFFER_SIZE)
+                if not request_data:  # The client sent an empty request
+                    client_socket.close()
+                    break
+
                 # If the client's request is bigger than BUFFER_SIZE, continue reading (until we've got \r\n\r\n)
                 while request_data[eol_index:] != eol_str:
                     request_data = request_data + client_socket.recv(BUFFER_SIZE)
+
             except socket.timeout:  # If a timeout has occurred
+                client_socket.close()
                 break
             # Our server shall print the request_data fully, with no other additions.
             decoded_request = request_data.decode()
             print(decoded_request)
-            connection_flag, file_name = get_connection_and_filename_from_request(decoded_request)
+            keep_alive = send_message_according_to_request(decoded_request, client_socket)
+            if not keep_alive:
+                client_socket.close()
+                break
 
 
-
-def get_connection_and_filename_from_request(data):
+def send_message_according_to_request(data, client_socket):
+    reply_msg = ""
+    content = ""
+    connection_state = ""
+    file_size = 0
     get_str = "GET"
     connection_str = "Connection:"
     result_html_str = "/result.html"
     redirect_str = "/redirect"
     index_html_str = "/index.html"
     redirection_flag = False
+    file_exist_flag = False
+
     data_lines = data.splitlines(keepends=True)
     for curr_line in data_lines:
         if curr_line.startswith(get_str):   # Some lines start with "GET [file] HTTP/1.1..."
@@ -63,18 +77,36 @@ def get_connection_and_filename_from_request(data):
                 file_path = file_path[1:]
             else:
                 file_path = "files" + file_path
+            file_exist_flag, content, file_size = file_handling(file_path)
 
-        elif curr_line.startswith(connection_str):
-            connection_flag = curr_line.split(" ")[1]
-            if redirection_flag:
+        if curr_line.startswith(connection_str):
+            connection_state = curr_line.split(" ")[1]  # close / Keep-alive according to the client's request
+            if redirection_flag:  # redirect ...
+                connection_state = "close"
+                reply_msg = "HTTP/1.1 301 Moved Permanently\r\nConnection: " + \
+                            connection_state + "\r\nLocation: /result.html" + "\r\n\r\n" + content.decode()
+                client_socket.send(reply_msg.encode())
 
+            elif file_exist_flag:  # The file exists.
+                reply_msg = "HTTP/1.1 200 OK\r\nConnection: " + connection_state +\
+                            "Content-Length: " + str(file_size) + "\r\n\r\n"
+                client_socket.send(b''.join([reply_msg.encode(), content]))
 
+            elif not file_exist_flag:  # The file doesn't exist.
+                connection_state = "close"
+                reply_msg = "HTTP/1.1 404 Not Found\r\nConnection: " + connection_state + "\r\n\r\n"
+                client_socket.send(reply_msg.encode())
 
-
+            if connection_state == "close":  # Close the client_socket.
+                # client_socket.close()
+                return False
+            else:
+                return True
 
 
 def file_handling(file_path):
     file_exist_flag = True
+    file_size = 0
     read_file = ""
     try:
         file = open(file_path, "rb")
@@ -83,7 +115,7 @@ def file_handling(file_path):
         file.close()
     except IOError:  # File doesn't exist!
         file_exist_flag = False
-    return file_exist_flag, read_file
+    return file_exist_flag, read_file, file_size
 
 
 if __name__ == '__main__':
